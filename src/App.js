8f3e467dbc8463abad463f489897e896f15d6a58
brutaina <brunatainact@gmail.com>
2023-07-05 10:04:49 -0300

    ppp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Cabecalho from './componentes/Cabecalho'
+import Rodapes from './componentes/Rodapes';
 
+
+// arquivo app jsx
+// componente funcional, porque ele é uma função
+//
 function App() {
+  // todo componente deve retornar um html, entre parenteses
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // o react substitui classname por class pq class é palavra reservada
+    <>
+
+    {/* nosso cabecalho tem uma propriedade chamada titulo */}
+    
+
+    <Cabecalho titulo = "Fav.io" subtitulo="O melhor gerenciador de favoritos da internet Brasileira!!!"/>
+    <Rodapes/>
+    </>
   );
 }
 
---
+++ src/componentes/Rodapes/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Rodapes() 
+{
+  return (
+    <div>rodapes
+
+    </div>
+  )
+}
