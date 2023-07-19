301528bfb46eac59f1fbf08ef66b3cf955fa0dad
brutaina <brunatainact@gmail.com>
2023-07-19 11:04:23 -0300

    att

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Cabecalho.module.css'
+
 export default function Cabecalho(props) {
   return (
-  
-  <>
-    <p className={styles.titulo}>{props.titulo}</p>
-    <h1>{props.subtitulo}</h1>
-    <img src={props.logo}/> 
-    </>
+    <div className={styles.cabecalho}>
+
+      <img src={props.logo} />
+      <p className={styles.titulo}>{props.titulo}</p>
+      <p className={`${styles.titulo} ${styles.subtitulo}`}>{props.subtitulo}</p>
+    </div>
   )
 }
